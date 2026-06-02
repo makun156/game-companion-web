3ee96577022b09d9dd7435b3077a306435e4a29f
一只软工考研er <894214654@qq.com>
2026-06-02 22:17:18 +0800

    实现游戏列表功能

---
+++ src/api/games/info/index.ts
@@ -0,0 +1,63 @@
+import request from '@/utils/request';
+import { AxiosPromise } from 'axios';
+import { GamesVO, GamesForm, GamesQuery } from '@/api/games/info/types';
+
+/**
+ * 查询游戏列表列表
+ * @param query
+ * @returns {*}
+ */
+
+export const listGames = (query?: GamesQuery): AxiosPromise<GamesVO[]> => {
+  return request({
+    url: '/games/list',
+    method: 'get',
+    params: query
+  });
+};
+
+/**
+ * 查询游戏列表详细
+ * @param id
+ */
+export const getGames = (id: string | number): AxiosPromise<GamesVO> => {
+  return request({
+    url: '/games/' + id,
+    method: 'get'
+  });
+};
+
+/**
+ * 新增游戏列表
+ * @param data
+ */
+export const addGames = (data: GamesForm) => {
+  return request({
+    url: '/games',
+    method: 'post',
+    data: data
+  });
+};
+
+/**
+ * 修改游戏列表
+ * @param data
+ */
+export const updateGames = (data: GamesForm) => {
+  return request({
+    url: '/games',
+    method: 'put',
+    data: data
+  });
+};
+
+/**
+ * 删除游戏列表
+ * @param id
+ */
+export const delGames = (id: string | number | Array<string | number>) => {
+  return request({
+    url: '/games/' + id,
+    method: 'delete'
+  });
+};
---
+++ src/api/games/info/types.ts
@@ -0,0 +1,102 @@
+export interface GamesVO {
+  /**
+   * 
+   */
+  id: string | number;
+
+  /**
+   * 游戏名称
+   */
+  name: string;
+
+  /**
+   * 游戏图标地址
+   */
+  icon: string;
+
+  /**
+   * 游戏图标地址Url
+   */
+  iconUrl: string;
+  /**
+   * 描述
+   */
+  description: string;
+
+  /**
+   * 游戏分类(参见字典sys_category)
+   */
+  category: string;
+
+  /**
+   * 排序
+   */
+  sort: number;
+
+  /**
+   * 状态(0-启用[默认] 1-禁用)
+   */
+  status: number;
+
+}
+
+export interface GamesForm extends BaseEntity {
+  /**
+   * 
+   */
+  id?: string | number;
+
+  /**
+   * 游戏名称
+   */
+  name?: string;
+
+  /**
+   * 游戏图标地址
+   */
+  icon?: string;
+
+  /**
+   * 描述
+   */
+  description?: string;
+
+  /**
+   * 游戏分类(参见字典sys_category)
+   */
+  category?: string;
+
+  /**
+   * 排序
+   */
+  sort?: number;
+
+  /**
+   * 状态(0-启用[默认] 1-禁用)
+   */
+  status?: number;
+
+}
+
+export interface GamesQuery extends PageQuery {
+
+  /**
+   * 游戏名称
+   */
+  name?: string;
+
+  /**
+   * 游戏分类(参见字典sys_category)
+   */
+  category?: string;
+
+  /**
+   * 状态(0-启用[默认] 1-禁用)
+   */
+  status?: number;
+
+  /**
+   * 日期范围参数
+   */
+  params?: any;
+}
